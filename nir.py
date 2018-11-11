82cf2f1c9e441d61a4fd41aa3855ec6269fbe3b0
DonMins <maks000156@yandex.ru>
2018-11-11 17:02:31 +0400

    eeg

--- nir.py
+++ nir.py
@@ -3,15 +3,45 @@ import pandas as pd
 import os
 import mne
 import os.path as op
-
-N = 11000
+import numpy as np
+N = 11001
 time = []
-# data_path = op.join(mne.datasets.sample.data_path(), 'MEG', 'sample')
-# raw = mne.io.read_raw_fif(op.join(data_path, 'sample_audvis_raw.fif'),
-#                           preload=True)
-# raw.set_eeg_reference('average', projection=True)  # set EEG average reference
-#
-# raw.plot(block=True, lowpass=40)
+
+
+
+# Numpy array of size 4 X 10000.
+
+path1 = "Shiz\\" + str(1) + ".txt"
+norm = pd.read_csv(path1, sep=" ", header=None)
+
+mas = np.zeros((16, N))
+
+
+for i in range(1,17):
+    allMax=0
+    mmax = abs(max(norm[i][0:N]))
+    mmin = abs(min(norm[i][0:N]))
+    allMax = max(mmax,mmin)
+
+    for j in range(N):
+        mas[i-1][j]=(10*(norm[i][j])/allMax)
+
+
+data = np.array([mas[0],mas[1],mas[2],mas[3],mas[4],mas[5],mas[6],mas[7],
+                 mas[8],mas[9],mas[10],mas[11],mas[12],mas[13],mas[14],mas[15]])
+# Definition of channel types and names.
+
+info = mne.create_info(
+    ch_names=['F7','F3','F4','F8','T3','C3','Cz','C4','T4','T5','P3','Pz','P4','T6','O1','O2'],
+    ch_types=['eeg','eeg','eeg','eeg','eeg','eeg','eeg','eeg','eeg','eeg','eeg','eeg','eeg','eeg','eeg','eeg'],
+    sfreq=400
+)
+raw = mne.io.RawArray(data, info)
+scalings = {'eeg': 16}
+
+calings = 'auto'  # Could also pass a dictionary with some value == 'auto'
+raw.plot(n_channels=16, scalings=scalings, title='Auto-scaled Data from arrays',
+         show=True, block=True)
 
 
 def average(data, number):
@@ -39,62 +69,62 @@ arrayNorm = []
 arrayShiz = []
 graficArrayNorm=[]
 graficArrayShiz=[]
-
-for i in range(1,40):
-
-    path1 = "Norm\\"+str(i)+".txt"
-    path2 = "Shiz\\"+str(i)+".txt"
-    tmp=0
-    tmp2=0
-    norm = pd.read_csv(path1, sep=" ", header=None)
-    shiz = pd.read_csv(path2, sep=" ", header=None)
-    for k in range(1,17):
-        arrayNorm.append(average(norm,k))
-        arrayShiz.append(average(shiz,k))
-    for j in range(16):
-        tmp = tmp + arrayShiz[j]
-        tmp2 = tmp2 + arrayNorm[j]
-    graficArrayShiz.append(tmp/len(arrayShiz))
-    graficArrayNorm.append(tmp2/len(arrayNorm))
-
-    print("Пациенты № "+str(i))
-    print("Норма                       Shiz ")
-    print("F7 = {0}    F7 = {1}:  ".format(arrayNorm[0],arrayShiz[0]))
-    print("F3 = {0}    F3 {1} :  ".format(arrayNorm[1],arrayShiz[1]))
-    print("F4 = {0}    F4 {1} :  ".format(arrayNorm[2],arrayShiz[2]))
-    print("F8 = {0}    F8 {1} :  ".format(arrayNorm[3],arrayShiz[3]))
-    print("T3 = {0}    T3 {1} :  ".format(arrayNorm[4],arrayShiz[4]))
-    print("C3 = {0}    C3 {1} :  ".format(arrayNorm[5],arrayShiz[5]))
-    print("Cz = {0}    Cz {1} :  ".format(arrayNorm[6],arrayShiz[6]))
-    print("C4 = {0}    C4 {1} :  ".format(arrayNorm[7],arrayShiz[7]))
-    print("T4 = {0}    T4 {1} :  ".format(arrayNorm[8],arrayShiz[8]))
-    print("T5 = {0}    T5 {1} :  ".format(arrayNorm[9],arrayShiz[9]))
-    print("P3 = {0}    P3 {1} :  ".format(arrayNorm[10],arrayShiz[10]))
-    print("Pz = {0}    Pz {1} :  ".format(arrayNorm[11],arrayShiz[11]))
-    print("P4 = {0}    P4 {1} :  ".format(arrayNorm[12],arrayShiz[12]))
-    print("T6 = {0}    T6 {1} :  ".format(arrayNorm[13],arrayShiz[13]))
-    print("O1 = {0}    O1 {1} :  ".format(arrayNorm[14],arrayShiz[14]))
-    print("O2 = {0}    O2 {1} :  ".format(arrayNorm[15],arrayShiz[15]))
-
-    norm=[]
-    shiz=[]
-    arrayNorm=[]
-    arrayShiz=[]
-
-
-# plt.figure(1)
-plt.figure("MRI_with_EEG")
-x = [i for i in range(1,40)]
-leg1, leg2=plt.plot(x,graficArrayNorm ,'o' ,x,graficArrayShiz, 'o' )
-plt.legend((leg1,leg2),("Norm","Shiz"))
-plt.grid(True)
-plt.show()
-
-plt.figure("MRI_with_EEG2")
-leg1, leg2=plt.plot(x,graficArrayNorm ,x,graficArrayShiz )
-plt.legend((leg1,leg2),("Norm","Shiz"))
-plt.grid(True)
-plt.show()
+#
+# for i in range(1,40):
+#
+#     path1 = "Norm\\"+str(i)+".txt"
+#     path2 = "Shiz\\"+str(i)+".txt"
+#     tmp=0
+#     tmp2=0
+#     norm = pd.read_csv(path1, sep=" ", header=None)
+#     shiz = pd.read_csv(path2, sep=" ", header=None)
+#     for k in range(1,17):
+#         arrayNorm.append(average(norm,k))
+#         arrayShiz.append(average(shiz,k))
+#     for j in range(16):
+#         tmp = tmp + arrayShiz[j]
+#         tmp2 = tmp2 + arrayNorm[j]
+#     graficArrayShiz.append(tmp/len(arrayShiz))
+#     graficArrayNorm.append(tmp2/len(arrayNorm))
+#
+#     print("Пациенты № "+str(i))
+#     print("Норма                       Shiz ")
+#     print("F7 = {0}    F7 = {1}:  ".format(arrayNorm[0],arrayShiz[0]))
+#     print("F3 = {0}    F3 {1} :  ".format(arrayNorm[1],arrayShiz[1]))
+#     print("F4 = {0}    F4 {1} :  ".format(arrayNorm[2],arrayShiz[2]))
+#     print("F8 = {0}    F8 {1} :  ".format(arrayNorm[3],arrayShiz[3]))
+#     print("T3 = {0}    T3 {1} :  ".format(arrayNorm[4],arrayShiz[4]))
+#     print("C3 = {0}    C3 {1} :  ".format(arrayNorm[5],arrayShiz[5]))
+#     print("Cz = {0}    Cz {1} :  ".format(arrayNorm[6],arrayShiz[6]))
+#     print("C4 = {0}    C4 {1} :  ".format(arrayNorm[7],arrayShiz[7]))
+#     print("T4 = {0}    T4 {1} :  ".format(arrayNorm[8],arrayShiz[8]))
+#     print("T5 = {0}    T5 {1} :  ".format(arrayNorm[9],arrayShiz[9]))
+#     print("P3 = {0}    P3 {1} :  ".format(arrayNorm[10],arrayShiz[10]))
+#     print("Pz = {0}    Pz {1} :  ".format(arrayNorm[11],arrayShiz[11]))
+#     print("P4 = {0}    P4 {1} :  ".format(arrayNorm[12],arrayShiz[12]))
+#     print("T6 = {0}    T6 {1} :  ".format(arrayNorm[13],arrayShiz[13]))
+#     print("O1 = {0}    O1 {1} :  ".format(arrayNorm[14],arrayShiz[14]))
+#     print("O2 = {0}    O2 {1} :  ".format(arrayNorm[15],arrayShiz[15]))
+#
+#     norm=[]
+#     shiz=[]
+#     arrayNorm=[]
+#     arrayShiz=[]
+#
+#
+# # plt.figure(1)
+# plt.figure("MRI_with_EEG")
+# x = [i for i in range(1,40)]
+# leg1, leg2=plt.plot(x,graficArrayNorm ,'o' ,x,graficArrayShiz, 'o' )
+# plt.legend((leg1,leg2),("Norm","Shiz"))
+# plt.grid(True)
+# plt.show()
+#
+# plt.figure("MRI_with_EEG2")
+# leg1, leg2=plt.plot(x,graficArrayNorm ,x,graficArrayShiz )
+# plt.legend((leg1,leg2),("Norm","Shiz"))
+# plt.grid(True)
+# plt.show()
 
 #
 # plt.figure(2)
